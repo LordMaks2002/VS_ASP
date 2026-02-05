8a7adb68358047e5cd8679b0a8cd458716c14dba
LordMaks2002 <124434358+LordMaks2002@users.noreply.github.com>
2026-02-05 19:19:00 +0200

    Add Project to Github

---
+++ WebApplication1/WebApplication1/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() // Contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Feedback feedback)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Feedback.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Feedback
+    {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="Need Your Name")]
+        public string Name { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Need Your Surname")]
+        public string SurName { get; set; }
+        [Display(Name = "Age")]
+        [Range(1, 220, ErrorMessage = "Age must be between 1 and 220")]
+        public int Age { get; set; }
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Display(Name = "Your Message")]
+        [Required(ErrorMessage = "Message cannot be empty")]
+        [StringLength(3000, ErrorMessage = "Message cannot exceed 3000 characters")]
+        public string Message { get; set; }
+
+    }
+}
